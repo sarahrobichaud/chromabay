86d0baa12f45ee51d73eb0829fe6ca34b81856b7
Sarah Robichaud <robichaudsarah@outlook.com>
2024-09-02 13:09:11 -0300

    Include source link

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Chromabay Test</h1>
+      <h1>Chromabay Test 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
